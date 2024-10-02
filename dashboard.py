94d4ee46633de0c15d62c4251eda512b662ce1dc
Zahra Maharani Putri <149281915+ZahraMaharaniP@users.noreply.github.com>
2024-10-02 12:09:01 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -130,10 +130,11 @@ st.pyplot(fig)
 
 # Fungsi untuk menampilkan jumlah peminjaman sepeda per hari
 def plot_daily_rentals(day_df):
-    plt.figure(figsize=(10, 5))  # Ukuran gambar
-    plt.rcParams['font.size'] = 12  # Ukuran font
+    plt.figure(figsize=(15, 5))  # Ukuran gambar yang lebih lebar
+    plt.rcParams['font.size'] = 12
 
     st.subheader("Bike Rental Chart")
+
     # Buat line plot dengan warna pink dan grid
     sns.lineplot(x='dteday', y='cnt', data=day_df, color='pink')
     plt.grid(True)
@@ -142,14 +143,20 @@ def plot_daily_rentals(day_df):
     plt.title('Jumlah Peminjaman Sepeda per Hari', fontsize=16, pad=20)
     plt.xlabel('Tanggal', fontsize=12)
     plt.ylabel('Jumlah Peminjaman', fontsize=12)
+
+    # Atur format tanggal dan interval
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30))   
+
+
+    # Batasi tampilan sumbu x (sesuaikan dengan data Anda)
+    # plt.xlim(pd.Timestamp('2011-01-01'), pd.Timestamp('2011-04-01'))
+
+    # Rotasi label sumbu x
     plt.xticks(rotation=45)
 
-    # Atur format tanggal pada sumbu x agar lebih jelas
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))  # Mengatur interval tanggal yang ditampilkan
-
-    st.pyplot(plt)  # Menampilkan plot di Streamlit
-    plt.clf()  # Bersihkan figure setelah ditampilkan
+    st.pyplot(plt)
+    plt.clf()
 
 # Panggil fungsi untuk menampilkan grafik peminjaman harian
 plot_daily_rentals(filtered_day_df)
