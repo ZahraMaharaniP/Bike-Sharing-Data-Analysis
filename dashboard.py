e14a2cabaff2322f31d8e9131319f5efcee97908
Zahra Maharani Putri <149281915+ZahraMaharaniP@users.noreply.github.com>
2024-10-02 13:19:31 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -74,8 +74,8 @@ st.pyplot(fig)
 
 # 3. Bike Rentals Over Time
 st.subheader("Bike Rentals Over Time")
-fig, ax = plt.subplots(figsize=(10, 5))  # Mengatur ukuran gambar menjadi 10 inci lebar dan 5 inci tinggi
-plt.rcParams['font.size'] = 12  # Mengatur ukuran font secara global menjadi 12
+fig, ax = plt.subplots(figsize=(10, 5))  # Set the figure size
+plt.rcParams['font.size'] = 12  # Set the global font size
 
 # Create line plot
 sns.lineplot(x='dteday', y='cnt', data=day_df, ax=ax, color='pink')
@@ -85,6 +85,12 @@ ax.grid(True)  # Show grid
 ax.set_title('Jumlah Peminjaman Sepeda per Hari', fontsize=16, pad=20)
 ax.set_xlabel('Tanggal', fontsize=12)
 ax.set_ylabel('Jumlah Peminjaman', fontsize=12)
+
+# Set x-axis major ticks to be every month
+ax.xaxis.set_major_locator(mdates.MonthLocator())  # Major ticks every month
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))  # Format to show month and year
+
+# Rotate x-ticks for better visibility
 ax.tick_params(axis='x', rotation=45)
 
 # Display the plot
